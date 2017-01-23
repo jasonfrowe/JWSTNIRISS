02dd55f3b1931397eb92342ae3cd1813752d624c
jasonfrowe <jasonfrowe@gmail.com>
2017-01-23 13:35:00 -0500

    Adding specgen routines

--- jwstniriss.py
+++ jwstniriss.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.insert(0, 'ramps2slopes/')
 sys.path.insert(0, 'trace/')
+sys.path.insert(0, 'specgen/')
 
 from astropy.io import fits #astropy modules for FITS IO
 def read_datacube(filename):
@@ -16,5 +17,17 @@ r2s=rs.r2s
 import trace as tr
 apertureflux=tr.apertureflux
 tracespec=tr.tracespec
+
+import specgen as sp
+readresponse=sp.readresponse
+readstarmodel=sp.readstarmodel
+readplanetmodel=sp.readplanetmodel
+is_number=sp.is_number
+p2w=sp.p2w
+w2p=sp.w2p
+addflux2pix=sp.addflux2pix
+ptrace=sp.ptrace
+genunconvolveimg=sp.genunconvolveimg
+convolveimg=sp.convolveimg
 
 
